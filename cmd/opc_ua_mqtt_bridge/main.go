6e903bf9f6862db8d7737402c1bd36bd8dc1b791
Max2000s <max.schoellkopf@gmx.de>
2024-10-28 20:19:28 +0100

    adjusted main for running application with configuration

--- cmd/opc_ua_mqtt_bridge/main.go
+++ cmd/opc_ua_mqtt_bridge/main.go
@@ -1,10 +1,23 @@
 package main
 
-import 
-(
-    "log"
+import (
+	"log"
+
+	"github.com/Max2000s/opc-ua-mqtt-bridge/pkg/app"
+	"github.com/Max2000s/opc-ua-mqtt-bridge/pkg/config"
 )
 
 func main() {
-   log.Printf("Starting OPC UA MQTT bridge")
+
+	CONFIG_FILE := "config/config.ylaml"
+
+	log.Printf("Start reading config file %s", CONFIG_FILE)
+	appConfig, err := config.LoadConfig(CONFIG_FILE)
+	if err != nil {
+		log.Fatalf("There was a problem reading the config file: %s", err)
+	}
+
+	app := app.NewApp(*appConfig)
+	app.Start()
+
 }
